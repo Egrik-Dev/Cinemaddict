9a1234df9763ea62405e7dfbb4d15da3585cd4e0
Egrik <egor-office-mail@yandex.ru>
2019-12-06 06:01:46 +0300

    Generated mocks and rewrote functions to render them

--- src/components/avatar.js
+++ src/components/avatar.js
@@ -1,5 +1,24 @@
-export const createAvatarTemplate = () => (`
+const countWatchedFilms = (films) => films.filter((film) => film.watched);
+
+const userRank = (filmsCount) => {
+  if (filmsCount === 0) {
+    return ``;
+  } else if (filmsCount >= 1 && filmsCount <= 10) {
+    return `novice`;
+  } else if (filmsCount > 10 && filmsCount <= 20) {
+    return `fan`;
+  } else {
+    return `movie buff`;
+  }
+};
+
+export const createAvatarTemplate = (films) => {
+  const numWatchedFilms = countWatchedFilms(films);
+  const rank = userRank(numWatchedFilms.length);
+  return (`
     <section class="header__profile profile">
-      <p class="profile__rating">Movie Buff</p>
+      <p class="profile__rating">${rank}</p>
       <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
     </section>`);
+};
+
